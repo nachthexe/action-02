0f04d7271949f1e2e6ce604cef719c7d43d6c714
John Son <40287191+son7211@users.noreply.github.com>
2021-06-01 22:28:02 +0900

    Update action02.js

--- action02.js
+++ action02.js
@@ -10,7 +10,7 @@ const octokit = new Octokit({
 
 
 octokit.request('POST /repos/{owner}/{repo}/issues/{issue_number}/labels', {
-    owner: 'company1111',
+    owner: 'exceeders',
     repo: 'action-02',
     issue_number: core.getInput('issue_number'),
     labels: [
